a3143f9d6a077c255d785174f866e32517458113
humstarman <lueyoung7@gmail.com>
2018-06-07 10:44:51 +0800

    update

---
+++ update-pem.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+set -e
+
+START=$(date +%s)
+FLAG=$1
+WAIT=3
+STAGE=0
+STAGE_FILE=stage.update
+if [ ! -f ./${STAGE_FILE} ]; then
+  INIT=0
+  touch ./${STAGE_FILE}
+  echo 0 > ./${STAGE_FILE} 
+  FLAG="--help"
+else
+  INIT=1
+fi
+if false; then
+INIT=1
+FLAG=$1
+if [[ "0" == "$INIT" ]]; then
+  sed -i s/"^INIT=0$"/"INIT=1"/g $0
+  FLAG="--help"
+fi
+fi
+function getScript(){
+  URL=$1
+  SCRIPT=$2
+  curl -s -o ./$SCRIPT $URL/$SCRIPT
+  chmod +x ./$SCRIPT
+}
+if [[ "-h" == "$FLAG" || "--help" == "$FLAG" ]]; then
+  echo " - Usage:"
+  echo " - This script is to update the permissions, including:"
+  echo " - CA"
+  echo " - ETCD"
+  echo " - Admin of Kubectl & kubeconfig file of Kubectl"
+  echo " - Flanneld"
+  echo " - Kubernetes"
+  echo " - Kubelet bootstrapping kubeconfig"
+  echo " - Kube-proxy & kubeconfig file of kube-proxy"
+  echo " -"
+  if [[ "0" == "$INIT" ]]; then
+    echo "---"
+    echo "---"
+    echo "---"
+    echo "If you run the script for the first time,"
+    echo "the help document shown for default."
+    echo "Re-run the script to function."
+    echo "---"
+  fi
+  sleep $WAIT
+  exit 0
+fi
+PROJECT="test-demo"
+# https://raw.githubusercontent.com/humstarman/test-demo-addons/master/coredns/coredns.yaml
+URL=https://raw.githubusercontent.com/humstarman/${PROJECT}-update-pem/master
+
+# 0 clear expired permission & check cfssl tool
+if [[ "$(cat ./${STAGE_FILE})" == "0" ]]; then
+  curl -s $URL/check-k8s-cluster.sh | /bin/bash 
+  curl -s $URL/check-ansible.sh | /bin/bash 
+  getScript $URL clear-expired-pem.sh
+  ansible all -m script -a ./clear-expired-pem.sh
+  curl -s $URL/check-cfssl.sh | /bin/bash 
+fi
+
+# 1 CA
+STAGE=$[${STAGE}+1]
+if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
+  curl -s $URL/update-ca-pem.sh | /bin/bash 
+  echo $STAGE > ./${STAGE_FILE}
+fi
+
+# 2 etcd 
+STAGE=$[${STAGE}+1]
+if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
+  curl -s $URL/update-etcd-pem.sh | /bin/bash 
+  echo $STAGE > ./${STAGE_FILE}
+fi
+
+# 3 kubectl 
+STAGE=$[${STAGE}+1]
+if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
+  curl -s $URL/update-kubectl-pem.sh | /bin/bash 
+  echo $STAGE > ./${STAGE_FILE}
+fi
+
+# 4 flanneld 
+STAGE=$[${STAGE}+1]
+if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
+  curl -s $URL/update-flanneld-pem.sh | /bin/bash 
+  echo $STAGE > ./${STAGE_FILE}
+fi
+
+# 5 kubernetes 
+STAGE=$[${STAGE}+1]
+if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
+  curl -s $URL/update-kubernetes-pem.sh | /bin/bash 
+  echo $STAGE > ./${STAGE_FILE}
+fi
+
+# 6 kubelet 
+STAGE=$[${STAGE}+1]
+if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
+  curl -s $URL/update-kubelet-pem.sh | /bin/bash 
+  echo $STAGE > ./${STAGE_FILE}
+fi
+
+# 7 kube-proxy 
+STAGE=$[${STAGE}+1]
+if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
+  curl -s $URL/update-kube-proxy-pem.sh | /bin/bash 
+  echo $STAGE > ./${STAGE_FILE}
+fi
+
+# 7 kube-proxy 
+STAGE=$[${STAGE}+1]
+if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
+  curl -s $URL/restart-svc.sh | /bin/bash 
+  echo $STAGE > ./${STAGE_FILE}
+fi
+
+# 8 clearance 
+STAGE=$[${STAGE}+1]
+if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - Kubernetes permmisson has been updated. "
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - clearance ... "
+  mkdir -p ./tmp
+  ls | grep -v -E "kube-install.sh|*.csv|${STAGE_FILE}|tmp" | xargs -I {} mv {} ./tmp
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - temporary files have been moved to ./tmp. "
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - you can delete ./tmp for free. "
+  echo $STAGE > ./${STAGE_FILE}
+fi
