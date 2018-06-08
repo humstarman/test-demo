45125ff2e10bcecd1ac5c05244c7642ac1dfcceb
humstarman <lueyoung7@gmail.com>
2018-06-08 15:14:37 +0800

    update

--- join-node.ba.sh
+++ join-node.ba.sh
@@ -64,71 +64,115 @@ URL=https://raw.githubusercontent.com/humstarman/${PROJECT}-impl/master
 TOOLS=${URL}/tools
 MAIN=${URL}/join-node
 
-# check environment 
+# 0 check environment 
 if [[ "$(cat ./${STAGE_FILE})" == "0" ]]; then
   curl -s $TOOLS/check-env.sh | /bin/bash 
   curl -s $TOOLS/check-ansible.sh | /bin/bash 
+  curl -s $TOOLS/mk-ansible-available.sh | /bin/bash
+  ## 1 shutdown selinux
+  curl -s -o ./shutdown-selinux.sh $TOOLS/shutdown-selinux.sh
+  ansible new -m script -a ./shutdown-selinux.sh
+  ## 2 stop firewall
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - stop firewall."
+  curl -s -o ./stop-firewall.sh $TOOLS/stop-firewall.sh
+  ansible new -m script -a ./stop-firewall.sh
+  ## 3 mkdirs
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - prepare directories."
+  curl -s $MAIN/batch-mkdir.sh | /bin/bash
 fi
 
-# 1 CA
+# 1 set env 
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $URL/update-ca-pem.sh | /bin/bash 
+  curl -s $MAIN/cluster-environment-variables.sh | /bin/bash 
   echo $STAGE > ./${STAGE_FILE}
 fi
 
-# 2 etcd 
+# 2 cp CA pem
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $URL/update-etcd-pem.sh | /bin/bash 
+  curl -s $MAIN/cp-ca-pem.sh | /bin/bash 
   echo $STAGE > ./${STAGE_FILE}
 fi
 
-# 3 kubectl 
+# 3 prepare kubernetes node componenets
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $URL/update-kubectl-pem.sh | /bin/bash 
+  curl -s $MAIN/cp-kubernetes-node-components.sh | /bin/bash 
   echo $STAGE > ./${STAGE_FILE}
 fi
 
-# 4 flanneld 
+# 4 cp kubectl 
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $URL/update-flanneld-pem.sh | /bin/bash 
+  curl -s $MAIN/cp-kubectl.sh | /bin/bash 
   echo $STAGE > ./${STAGE_FILE}
 fi
 
-# 5 kubernetes 
+# 5 deploy flanneld 
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $URL/update-kubernetes-pem.sh | /bin/bash 
+  curl -s $URL/deploy-flanneld.sh | /bin/bash 
   echo $STAGE > ./${STAGE_FILE}
 fi
 
-# 6 kubelet 
+# 5 deploy node 
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $URL/update-kubelet-pem.sh | /bin/bash 
+  curl -s $URL/deploy-node.sh | /bin/bash 
   echo $STAGE > ./${STAGE_FILE}
 fi
 
-# 7 kube-proxy 
-STAGE=$[${STAGE}+1]
-if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $URL/update-kube-proxy-pem.sh | /bin/bash 
-  echo $STAGE > ./${STAGE_FILE}
-fi
-
-# 8 restart services 
-STAGE=$[${STAGE}+1]
-if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $URL/restart-svc.sh | /bin/bash 
-  echo $STAGE > ./${STAGE_FILE}
-fi
-
-# 9 clearance 
+# 6 clearance 
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
   curl -s $TOOLS/clearance.sh | /bin/bash
   echo $STAGE > ./${STAGE_FILE}
 fi
+
+# ending
+[ -z "$N_NODE" ] && N_NODE=0
+TOTAL=$[${N_MASTER}+${N_NODE}]
+END=$(date +%s)
+ELAPSED=$[$END-$START]
+MINUTE=$[$ELAPSED/60]
+NEW=$(sed s/","/" "/g ./new.csv)
+N_NEW=$(echo $NEW | wc -w)
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - summary: "
+if [[ "1" == "N_NEW" ]]; then
+  echo " - add only one new node into Kubernetes cluster elapsed: $ELAPSED sec, approximately $MINUTE ~ $[$MINUTE+1] min."
+else
+  echo " - add $N_NEW new nodes into Kubernetes cluster elapsed: $ELAPSED sec, approximately $MINUTE ~ $[$MINUTE+1] min."
+fi
+echo " - Previous Kubernetes paltform: "
+echo " - Total nodes: $TOTAL"
+echo " - With masters: $N_MASTER"
+echo " --- "
+TOTAL=$[${TOTAL}+${N_NEW}]
+echo " - Current Kubernetes paltform: "
+echo " - Total nodes: $TOTAL"
+echo " - With masters: $N_MASTER"
+FILE=approve-pem.sh
+if [ ! -f "$FILE" ]; then
+  cat > $FILE << EOF
+#!/bin/bash
+CSRS=\$(kubectl get csr | grep Pending | awk -F ' ' '{print \$1}')
+if [ -n "\$CSRS" ]; then
+  for CSR in \$CSRS; do
+    kubectl certificate approve \$CSR
+  done
+fi
+EOF
+  chmod +x $FILE
+fi
+echo " - For a little while, use the script ./$FILE to approve kubelet certificate."
+echo " - use 'kubectl get csr' to check the register."
+## re-set env
+curl -s $TOOLS/re-set-env.sh | /bin/bash
+## make backup
+THIS_DIR=$(cd "$(dirname "$0")";pwd)
+nohup curl -s $TOOLS/mk-backup.sh | /bin/bash
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - backup import info from $THIS_DIR to /var/k8s/bak."
+echo " - this process runs in backgroud, so no need to wait."
+sleep $WAIT
+exit 0
