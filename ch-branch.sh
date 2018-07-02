0a5ce24312aabbe95de9e0bd98af5dcef91c8201
humstarman <lueyoung7@gmail.com>
2018-07-02 09:25:54 +0800

    update

--- ch-branch.sh
+++ ch-branch.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -e
+if false; then
 B=$1
 if [ -z "$B" ]; then
   echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [ERROR] - input the branch wanted to be replaced !!!"
   sleep 3
   exit 1
 fi
+fi
+THIS_BRANCH=$(git branch | grep "*")
+THIS_BRANCH=${THIS_BRANCH##*" "}
 THIS_FILE=$0
 THIS_FILE=${THIS_FILE##*/}
 TARGET="BRANCH="
-find ./ -type f | grep -v $THIS_FILE |xargs sed -i "/$TARGET/ c ${TARGET}${B}"
-echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - branch change to ${B}"
+find ./ -type f | grep -v $THIS_FILE |xargs sed -i "/$TARGET/ c ${TARGET}${THIS_BRANCH}"
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - branch change to ${THIS_BRANCH}"
 exit 0
-
