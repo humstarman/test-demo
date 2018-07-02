625b195b9eb8ec1076da2cb6196471a08a6a0bb7
humstarman <lueyoung7@gmail.com>
2018-07-02 13:25:45 +0800

    update

--- add-master.sh
+++ add-master.sh
@@ -68,7 +68,7 @@ while getopts "h" opt; do
     esac
 done
 PROJECT="test-demo"
-BRANCH=master
+BRANCH=v1.10
 URL=https://raw.githubusercontent.com/humstarman/${PROJECT}-impl/${BRANCH}
 TOOLS=${URL}/tools
 THIS_FILE=$0
